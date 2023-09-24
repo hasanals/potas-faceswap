cae31a9d098b24b3c6d4e9474748d635ce080ccb
Luis C <me@lucataco.com>
2023-09-23 21:22:24 -0400

    Fixes

--- download.py
+++ download.py
@@ -13,6 +13,10 @@ def download_model():
     # Download inswapper_128.onnx
     os.system("cd cache && wget http://43.153.104.112:814/inswapper_128.onnx.zip && unzip inswapper_128.onnx.zip")
 
+    # Caches insight files
+    os.system("mkdir -p /opt/conda/lib/python3.8/site-packages/insightface/models/buffalo_l/")
+    os.system("wget https://github.com/deepinsight/insightface/releases/download/v0.7/buffalo_l.zip")
+    os.system("unzip buffalo_l.zip -d /opt/conda/lib/python3.8/site-packages/insightface/models/buffalo_l")
 
 if __name__ == "__main__":
     download_model()
